66d5318c277f21d5d9be49f4b77942ece0f39bdc
MaZen <MazenAnter>
2024-10-08 19:28:08 +0300

    add styles to app

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color baseColor = Color(0xffE23E3E);
+  static const Color darkGrayColor = Color(0xffA9A9A9);
+  static const Color grayColor = Color(0xffC1C1C1);
+}
---
+++ lib/utils/app_text_styles.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+abstract class AppTextStyles {
+  static const TextStyle syleSemiBold30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle syleSemiBold20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle syleSemiBold24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle syleSemiBold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle syleSemiBold11 = TextStyle(
+    fontSize: 11,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle syleRegular20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle syleRegular11 = TextStyle(
+    fontSize: 11,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle syleRegular8 = TextStyle(
+    fontSize: 8,
+    fontWeight: FontWeight.w400,
+    color: Color(0xffA9A9A9),
+  );
+
+  static const TextStyle syleRegular14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: Color(0xffA9A9A9),
+  );
+}
